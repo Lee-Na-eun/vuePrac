2c4c54b9bb32f1eac92ab0a3f804135af6bceb13
Lee Naeun <nanni4287@gmail.com>
2022-05-23 06:42:14 +0900

    fix: allow user to see incrrect match before hiding

--- src/App.vue
+++ src/App.vue
@@ -62,14 +62,14 @@ export default {
     cardItems.forEach((item) => {
       cardList.value.push({
         value: item,
-        visible: true,
+        visible: false,
         position: null,
         matched: false,
       });
 
       cardList.value.push({
         value: item,
-        visible: true,
+        visible: false,
         position: null,
         matched: false,
       });
@@ -103,8 +103,10 @@ export default {
             cardList.value[cardOne.position].matched = true;
             cardList.value[cardTwo.position].matched = true;
           } else {
-            cardList.value[cardOne.position].visible = false;
-            cardList.value[cardTwo.position].visible = false;
+            setTimeout(() => {
+              cardList.value[cardOne.position].visible = false;
+              cardList.value[cardTwo.position].visible = false;
+            }, 2000);
           }
           useSelection.value.length = 0;
         }
